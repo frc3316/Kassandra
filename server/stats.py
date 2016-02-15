641029a8c4701f39c0018733c2aa3641d1010706
Barak Shelef <shelef@guardicore.com>
2016-02-15 23:32:30 +0200

    Adjusted shooting stats to fit UI API

--- server/stats.py
+++ server/stats.py
@@ -1,5 +1,5 @@
-SIZE_LIST = ['large', 'medium', 'small']
-COLOR_LIST = ['green', 'orange', 'red']
+SIZE_LIST = ['3', '2', '1']
+COLOR_LIST = ['success', 'warning', 'danger']
 
 class Granulator(object):
 	def __init__(self, thershold_list, ret_list):
@@ -41,78 +41,51 @@ class GoalsScored(StatsHandler):
 	HIGH_PERCENTAGE = 0.75
 	MED_PERCENTAGE = 0.25
 	PERCENTAGE_GRANULATOR = Granulator([HIGH_PERCENTAGE, MED_PERCENTAGE], COLOR_LIST)
+	HIGH_VARIANCE = 3
+
+	STATS_STRUCT = dict(successful='0',
+			            attempted='0',
+			            size=SIZE_LIST[-1],
+			            color=COLOR_LIST[-1],
+			            variant=False,
+			            teams=[])
+
+	@staticmethod
+	def _variance(values):
+		average = sum(values) / en(values)
+		return sum(map(lambda v: (v - average) ** 2, values)) / 2
 
 	@staticmethod
 	def _run_stats(success, failure):
-		match_number = len(success)
-		if match_number == 0:
-			return dict(match_number=match_number)
-			
+		stats = STATS_STRUCT.copy()
+
+		amount = len(success)
 		total_scored = float(sum(success))
 		total_shots = float(sum(failure) + sum(success))
-		if total_shots == 0:
-			return dict(match_number=match_number,
-			            total_scored=total_scored,
-			            total_shots=total_shots)
+		if amount == 0 or total_shots == 0:
+			return stats
 			
-		shooting_percentage = total_scored / total_shots
-		average_scores_per_game = total_scored / match_number
-		average_shots_per_game = total_shots / match_number
-
-		size = self.AMOUNT_GRANULATOR.get(total_shots)
-		color = self.PERCENTAGE_GRANULATOR.get(shooting_percentage)
+		stats['successful'] = '%.2f' % (total_scored / amount)
+		stats['attempted'] = '%.2f' % (total_shots / amount)
+		stats['size'] = self.AMOUNT_GRANULATOR.get(total_shots)
+		stats['color'] = self.PERCENTAGE_GRANULATOR.get(total_scored / total_shots)
+		stats['variant'] = self.HIGH_VARIANCE < GoalsScored._variance([float(s) / (s + f) for (s, f) in zip(success, failure)])
 		
-		return dict(match_number=match_number,
-		            total_scored=total_scored,
-					total_shots=total_shots,
-					shooting_percentage=shooting_percentage,
-					average_scores_per_game=average_scores_per_game,
-					average_shots_per_game=average_shots_per_game,
-					size=size,
-					color=color)
+		return stats
 		
 	def filter(self, match_data):
 		shooting_data = [match['shooting'] for match in match_data]
 		
 		low_far_success = [match['low']['far']['success'] for match in shooting_data]
 		low_far_failure = [match['low']['far']['failure'] for match in shooting_data]
-		low_far_stats = self._run_stats(low_far_success, low_far_failure)
-		
 		high_far_success = [match['high']['far']['success'] for match in shooting_data]
 		high_far_failure = [match['high']['far']['failure'] for match in shooting_data]
-		high_far_stats = self._run_stats(high_far_success, high_far_failure)
-		
-		far_stats = self._run_stats(map(sum, zip(low_far_success, high_far_success)),
-									map(sum, zip(low_far_failure, high_far_failure)))
-		
 		low_close_success = [match['low']['close']['success'] for match in shooting_data]
 		low_close_failure = [match['low']['close']['failure'] for match in shooting_data]
-		low_close_stats = self._run_stats(low_close_success, low_close_failure)
-		
 		high_close_success = [match['high']['close']['success'] for match in shooting_data]
 		high_close_failure = [match['high']['close']['failure'] for match in shooting_data]
-		high_close_stats = self._run_stats(high_close_success, high_close_failure)
 		
-		close_stats = self._run_stats(map(sum, zip(low_close_success, high_close_success)),
-									  map(sum, zip(low_close_failure, high_close_failure)))
-		
-		low_stats = self._run_stats(map(sum, zip(low_close_success, low_far_success)),
-									map(sum, zip(low_close_failure, low_far_failure)))
-		high_stats = self._run_stats(map(sum, zip(high_close_success, high_far_success)),
-									 map(sum, zip(high_close_failure, high_far_failure)))
-		
-		total_stats = self._run_stats(map(sum, zip(low_close_success, low_far_success, high_close_success, high_far_success)),
-									 map(sum, zip(low_close_failure, low_far_failure, high_close_failure, high_far_failure)))
-		
-		return dict(low_far_stats=low_far_stats,
-		            high_far_stats=high_far_stats,
-					low_close_stats=low_close_stats,
-					high_close_stats=high_close_stats,
-					far_stats=far_stats,
-					close_stats=close_stats,
-					low_stats=low_stats,
-					high_stats=high_stats,
-					total_stats=total_stats)
-		
-		             
-		
+		return dict(low=dict(far=GoalsScored._run_stats(low_far_success, low_far_failure),
+			                 close=GoalsScored._run_stats(low_close_success, low_close_failure)),
+		            high=dict(far=GoalsScored._run_stats(high_far_success, high_far_failure),
+		            	      close=GoalsScored._run_stats(high_close_success, high_close_failure)))
