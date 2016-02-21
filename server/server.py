dda022bce887aaf4e4b09f607033f0881ad6e9ca
Barak Shelef <shelef@guardicore.com>
2016-02-21 19:36:13 +0200

    Refactored data fetching to work with db

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects import postgresql
+from collections import defaultdict
 import traceback
 import glob
 import json
@@ -13,6 +14,7 @@ from stats import statsmgr
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 DATABASE_DIR = "database"
@@ -38,6 +40,10 @@ class Match(db.Model):
         for key, value in teams_dict.items():
             setattr(self, key, int(value))
 
+    def to_dict(self):
+        return {self.match: {'red': [self.red1, self.red2, self.red3],
+                             'blue': [self.blue1, self.blue2, self.blue3]}}
+
     def __repr__(self):
         return '<Match %r>' % self.match
 
@@ -54,9 +60,14 @@ class MatchDefence(db.Model):
         self.match = match
         self.tactic = tactic
 
+    def to_dict(self):
+        return {'defender': self.defender,
+                'attacker': self.attacker,
+                'match': self.match,
+                'tactic': self.tactic}
+
     def __repr__(self):
         return "<MatchDefence [%d on %d] [Match: %s]>" % (self.defender, self.attacker, self.match)
-
 
 class MatchStats(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -113,6 +124,7 @@ class MatchStats(db.Model):
         self.match = match
         self.team = team
         
+        # TODO: This doesn't verify structure. which is slightly bad.
         for key, breach_data in breaching_dict.items():
             for result, value in breach_data.items():
                 setattr(self, '%s_%s' % (key, result), int(value))
@@ -136,6 +148,41 @@ class MatchStats(db.Model):
             defences.append(match_defence.id)
 
         self.defences = defences
+
+    def to_dict(self):
+        breaching_dict = {}
+        for key in ('a1', 'a2', 'b1', 'b2', 'c1', 'c2', 'c1_assist', 'c2_assist', 'd1', 'd2', 'lb'):
+            breaching_dict[key] = {}
+            for result in ('success', 'failure'):
+                breaching_dict[key][result] = getattr(self, '%s_%s' % (key, result))
+
+        scoring_dict = {}
+        for goal in ('low', 'high'):
+            scoring_dict[goal] = {}
+            for distance in ('far', 'close'):
+                scoring_dict[goal][distance] = {}
+                for result in ('success', 'failure'):
+                    scoring_dict[goal][distance][result] = getattr(self, '%s_%s_%s' % (goal, distance, result))
+
+        collection_dict = {}
+        for key in ('floor', 'hp'):
+            collection_dict[key] = getattr(self, key)
+
+        end_game_dict = {}
+        for key in ('challenge', 'scale'):
+            end_game_dict[key] = getattr(self, key)
+
+        defences_list = []
+        for defence in MatchDefence.query.filter(MatchDefence.id.in_(self.defences)).all():
+            defences_list.append({'team': defence.attacker, 'tactic': defence.tactic})
+
+        return {'team': self.team,
+                'match': self.match,
+                'breaching': breaching_dict,
+                'shooting': shooting_dict,
+                'collection': collection_dict,
+                'end_game': end_game_dict,
+                'defences': defences_list}
         
 
     def __repr__(self):
@@ -164,24 +211,28 @@ def _db_add_match_stats(match_stats_data):
     db.session.commit()
     return match_stats_object
 
-def _db_get_team_data(team):
+def _db_get_match_stats(team=None, match=None):
     """ Fetch team's match datas from DB """
-    return [json.load(open(match_file)) for match_file in glob.glob(os.path.join(DATABASE_DIR, '*_%s.json' % team))]
+    query = MatchStats.query
+    if team is not None:
+        query = query.filter(MatchStats.team == team)
+    if match is not None:
+        query = query.filter(MatchStats.match == match)
+    return [stats.to_dict() for stats in query.all()]
 
 def _db_get_matchs():
     """ Fetch match list """
     matches = {}
     for m in Match.query.all():
-        matches[m.match] = {'red': [m.red1, m.red2, m.red3],
-                            'blue': [m.blue1, m.blue2, m.blue3]}
+        matches.update(m.to_dict())
+
     return matches
 
 def _db_get_match(match):
     """ Fetch specific match alliances  """
     matches = {}
     for m in Match.query.filter_by(match=match):
-        return {'red': [m.red1, m.red2, m.red3],
-                'blue': [m.blue1, m.blue2, m.blue3]}
+        return m.to_dict().get(match)
 
 ## Flask Server Routes
 @app.route('/add/match', methods=['GET', 'POST'])
@@ -228,11 +279,34 @@ def add_match_stats():
     except:
         sys.stderr.write(traceback.format_exc() + "\n")
 
+@app.route('/stats')
+def get_team_stats_list():
+    """ return list of teams and matches and how much information was collected on them """
+    try:
+        collected_stats = _db_get_match_stats()
+        match_list = _db_get_matchs()
+        stats_by_team = defaultdict(int)
+        stats_by_match = defaultdict(lambda: {'red': 0, 'blue': 0})
+        for match_stats in collection_dict:
+            team = match_stats.team
+            match = match_stats.match
+            alliance = 'red' if team in match_list[match]['red'] else 'blue'
+
+            stats_by_team[team] += 1
+            stats_by_match[match][alliance] += 1
+
+        teams = sorted(stats_by_team.items())
+        matches = sorted(stats_by_match.items())
+
+        return jsonify(status='OK', teams=teams, matches=matches)
+    except:
+        sys.stderr.write(traceback.format_exc() + "\n")
+
 @app.route('/stats/team/<int:team_number>')
 def get_team_stats(team_number):
     """ gets a team's statistics """
     try:
-        matches = _db_get_team_data(team_number)
+        matches = _db_get_match_stats(team=team_number)
         if not matches:
             return jsonify(status='ERROR', team_number=team_number,
                            msg=("No matches for team %d." % team_number))
@@ -258,7 +332,7 @@ def get_alliance_stats(match, alliance):
         
         matches = []
         for team_number in alliance_teams:
-            matches.extend(_db_get_team_data(team_number))
+            matches.extend(_db_get_match_stats(team=team_number))
         if not matches:
             return jsonify(status='ERROR', match=match, alliance=alliance,
                            msg=("No matches for teams %d, %d, %d." % tuple(alliance_teams)))
